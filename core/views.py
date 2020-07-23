ab840b4b8afc12c6d9ebf3a29c5ac5eb7b6e6901
Sara Cristina Ferreira <ferreirasara1501@hotmail.com>
2020-07-23 13:34:53 -0300

    Status.

--- core/views.py
+++ core/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-
+from .models import Status
 
 def index(request):
-    return render(request, 'index.html')
+    status = Status.objects.all()
+
+    context = {
+        'status': status
+    }
+
+    return render(request, 'index.html', context)
+
 
 def dashboard(request):
     return render(request, 'dashboard.html')
+
 
 def settings(request):
     return render(request, 'settings.html')
