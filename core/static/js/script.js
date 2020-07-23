e0411f8d4fd2ebc299a6585f27471865e2eaac71
Sara Cristina Ferreira <ferreirasara1501@hotmail.com>
2020-07-22 23:18:33 -0300

    Layout.

--- core/static/js/script.js
+++ core/static/js/script.js
@@ -1,3 +1,10 @@
-function selectAll() {
-    alert('teste');
-}
+var thCheck = document.getElementById("thCheck");
+var rows = document.getElementById("resultTable").rows;
+document.getElementById("").onclick = function () {
+    if (thCheck.checked) {
+        alert(rows[0].cells[0].childNodes.checked);
+        rows[0].cells[0].checked = true;
+    } else {
+        rows[0].cells[0].checked = false;
+    }
+};
